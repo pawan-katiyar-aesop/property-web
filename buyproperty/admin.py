8548e811eb05a18c50caa1016734600f9fcb8044
Shivendra <shivendra@Himanshu-MacBook-Air.local>
2018-10-24 19:02:03 +0530

    fixed JSON field

--- buyproperty/admin.py
+++ buyproperty/admin.py
@@ -1,11 +1,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from buyproperty.models import Property, Owner, Address, Landmark, Leads
+from buyproperty.models import Property, Users, Address, Landmark, Leads, FloorPlan
 
 
 admin.site.register(Property)
-admin.site.register(Owner)
+admin.site.register(Users)
 admin.site.register(Address)
 admin.site.register(Landmark)
 admin.site.register(Leads)
+admin.site.register(FloorPlan)
