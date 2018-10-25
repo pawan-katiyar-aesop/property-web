900d186b9d585b94b8547fb394ccf21fb7041571
Shivendra <shivendra@Himanshu-MacBook-Air.local>
2018-10-25 16:57:11 +0530

    model updated

--- property/settings.py
+++ property/settings.py
@@ -41,13 +41,13 @@ BASE_URL = "http://localhost:8000"
 # Application definition
 
 INSTALLED_APPS = [
+    'buyproperty',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'buyproperty'
 ]
 
 MIDDLEWARE = [
