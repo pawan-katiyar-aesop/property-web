ef62e8598c0ad0e0a80b38ad5ba57572ca87cc60
pawan-katiyar-aesop <pawan@sqre1.com>
2018-10-31 18:06:26 +0530

    Home page created.

--- buyproperty/urls/api.py
+++ buyproperty/urls/api.py
@@ -9,9 +9,7 @@ urlpatterns = [
     url(r'^address/$', views.ListCreateAddressAPIView.as_view(), name="list-create-property"),
     url(r'^address/(?P<pk>\d+)/$', views.RetrieveUpdateDestroyAddressAPIView.as_view(), name="retrieve-update-address"),
     url(r'^property/(?P<pk>\d+)/$', views.RetrieveUpdateDestroyPropertyAPIView.as_view(), name="retrieve-update-property"),
-    url(r'^country_codes/$', views.CountryCodeListView.as_view(), name="retrieve-update-property"),
-    url(r'^property/top/$', views.RetrieveTopPropertyView.as_view(), name="retrieve-top-property"),
-    url(r'^property/(?P<key>\w+)/$', views.SearchResultApiView.as_view(), name="retrieve-top-property")
+    url(r'^country_codes/$', views.CountryCodeListView.as_view(), name="retrieve-update-property")
 
 
 ]
