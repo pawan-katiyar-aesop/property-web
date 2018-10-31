469de7991815c378bd0ab4f0af9ecd7b67d6d6c0
pawan-katiyar-aesop <pawan@sqre1.com>
2018-10-31 15:27:52 +0530

    url created.

--- website/views.py
+++ website/views.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import status
+from django.views.generic import TemplateView
+from django.views import generic
+from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = "../templates/home.html"
