e01fd0db6a667bcc8ae2979d64b9706ca0ebfac7
Gaurav Baliyan <gauravskt22@gmail.com>
2018-11-01 16:53:14 +0530

    video urls

--- property/static/theme/js/property-create.js
+++ property/static/theme/js/property-create.js
@@ -76,6 +76,7 @@ let property_create_app = new Vue({
         propertyImages:{
             imageList: [],
         },
+        propertyVideos: [],
         videoTours:{
             list: [],
         },
@@ -191,7 +192,16 @@ let property_create_app = new Vue({
 
         },
         addVideoUrlFields:function(){
-
+            let that = this;
+            let urls_dict = {};
+            const index = generate_unique_number();
+            urls_dict["url"+index] = "";
+            urls_dict["index"] = index;
+            that.propertyVideos.push(urls_dict);
+        },
+        removeVideoUrlFields:function(index){
+            let that = this;
+            that.propertyVideos.splice(index, 1)
         },
         populateCharges :function(){
             let that=this;
