06453f46b50581fee57128cb7f304546fae65962
pawan-katiyar-aesop <pawan@sqre1.com>
2018-11-02 14:23:13 +0530

    about and contact page added.

--- website/urls/urls.py
+++ website/urls/urls.py
@@ -4,6 +4,8 @@ from website import views
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name="website-home-page"),
     url(r'^property-listing/$', views.PropertyListView.as_view(), name="website-listing"),
-    url(r'^property-details/$', views.PropertyDetailsView.as_view(), name="website-details")
+    url(r'^property-details/$', views.PropertyDetailsView.as_view(), name="website-details"),
+    url(r'^about/$', views.AboutView.as_view(), name="website-about"),
+    url(r'^contact/$', views.ContactView.as_view(), name="website-contact")
 
 ]
