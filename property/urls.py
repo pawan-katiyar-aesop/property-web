0fa2b258ea672327634c409f953292572132fc03
shivendra2407 <shivendrasrivastava19@gmail.com>
2018-11-04 19:23:30 +0530

    Added urls for media and static roots to urls.py main file

--- property/urls.py
+++ property/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -24,7 +26,9 @@ urlpatterns = [
     url(r'^api/real_estate/', include('website.urls.api'), name="website-api"),
 
 ]
-# if settings.DEBUG:
-#
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
