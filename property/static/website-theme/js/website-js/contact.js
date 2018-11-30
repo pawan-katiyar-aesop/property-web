c80d9e37d24b1601dfd46af27ebdb5c649d492fb
pawan-katiyar-aesop <pawan@sqre1.com>
2018-11-30 14:14:40 +0530

    button disabled

--- property/static/website-theme/js/website-js/contact.js
+++ property/static/website-theme/js/website-js/contact.js
@@ -4,39 +4,39 @@ let website_contact_app = new Vue({
         contactName: '',
         contactEmail: '',
         contactMessage: '',
-        partnerType: 'owner'
+        partnerType: 'owner',
+        togg: false
 
     },
     methods: {
         sendEmail: function () {
             let that = this;
-            if (!that.contactName && !that.contactEmail && !that.contactMessage){
+            if (!that.contactName || !that.contactEmail || !that.contactMessage){
                 show_notification("danger", "Please fill all information!");
             }
-            let body = {
-                "name": that.contactName,
-                "email": that.contactEmail,
-                "message": that.contactMessage,
-                "partner_type": that.partnerType
-            };
-            axios.post('/api/customer_leads/', body)
-            .then(function (response) {
-                // alert("Your request has been received.");
+            else{
+                let body = {
+                    "name": that.contactName,
+                    "email": that.contactEmail,
+                    "message": that.contactMessage,
+                    "partner_type": that.partnerType
+                };
+                axios.post('/api/customer_leads/', body)
+                .then(function (response) {
 
-                show_notification("success", "Thank you for your message. We will connect with you shortly.");
+                    show_notification("success", "Thank you for your message. We will connect with you shortly.");
 
-            })
-            .catch(function (response) {
-                // alert("Invalid Email!");
-                show_notification("danger", "A fatal error occurred, and this page might not function correctly.");
-            })
+                })
+                .catch(function (response) {
+                    show_notification("danger", "Invalid Email!");
+                })
+            }
         }
     },
     watch: {
 
     },
     mounted() {
-
 
     },
     computed: {
